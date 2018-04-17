da6af21f3ddfde4be3918628099837e4133f6816
tuotomello <tuotomello@gmail.com>
2018-04-17 09:28:18 -0300

    Paper

---
+++ Paper_Initialization.m
@@ -0,0 +1,125 @@
+clear; close all;
+
+%**************************************************
+% INITIALIZATION - WFS DATA
+%**************************************************
+r0 = 0.2;
+L0 = 50;
+f0 = 1/L0;
+lambda = 0.5e-6;
+frac = [0.78,0.10,0.12];  %-> Tokovinin2006
+%frac = [1,1,1];
+tel_diam = 8.0; %Telescope Diameter (m)
+ang_dist = 30;  %Angular distance between outer guide stars (arcsec)
+WFS_angles = ang_dist*[[sin(0),cos(0)]; [sin(72*pi/180),cos(72*pi/180)]; [sin(144*pi/180),cos(144*pi/180)]; [sin(216*pi/180),cos(216*pi/180)]; [sin(288*pi/180),cos(288*pi/180)]];   %for each WFS, in x,y
+WFS_target = [0,0];
+
+for size_i = 7:10    
+%for size_i = 1:10    
+    WFS_size = size_i*6+5;   %Number of subapertures in diameter (Only odd numbers)
+    fprintf('###########################################\nBeginning size_i: %d, WFS size: %d\n\n',size_i,WFS_size);
+
+    subap_mask = ones(WFS_size);
+
+
+    subap_index = find(subap_mask); %index of valid subapertures
+    n_subaps = length(subap_index); %number of valid subapertures
+
+    %**************************************************
+    % INITIALIZATION - TURBULENCE DATA
+    %**************************************************
+    altitudes = [0.,4000.,8000.];     %layers altitudes (m) -> Tokovinin2006
+    layer_size = WFS_size;
+    alt_p = altitudes/(tel_diam/layer_size);  %layer altitudes relative to pixel size (pixels)
+    shifts = tan(ang_dist*pi/648000)*alt_p;        %maximum shift in each layer
+    extra_p = 2*ceil(shifts);        %extra pixels for layer movement
+    extra_p = extra_p+mod(extra_p,2);    %must be even
+    sizes = extra_p+layer_size;
+
+
+    %**************************************************
+    % CREATE COVARIANCE MATRICES
+    %**************************************************
+    c = (24/5*gamma(6/5))^(5/6)*gamma(11/6)/(2^(5/6)*pi^(8/3))*(r0*f0)^(-5/3);
+
+    fprintf('Creating Covariance Matrices\nMatrix:                 ');
+    for i = 1:length(sizes)
+        X_index = meshgrid(1:sizes(i));
+        Y_index = X_index';
+        Indexes = [reshape(X_index,1,[]); reshape(Y_index,1,[])];
+        mindist = 1e-6; %Avoid zeros
+        Cov_M{i} = zeros(sizes(i)^2);
+        for lin = 1:sizes(i)^2
+            fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b%01d line %04d/%04d',i,lin,sizes(i)^2);
+            for col = 1:sizes(i)^2
+                %Distance
+                dist = sqrt(sum((Indexes(:,col) - Indexes(:,lin)).^2));
+                dist = dist*tel_diam/layer_size + mindist;  %distance between voxels
+                Cov_M{i}(lin,col) = c*(2*pi*dist*f0)^(5/6)*besselk(5/6,2*pi*f0*dist);
+            end
+        end
+    end
+    fprintf('\n\n');
+
+
+
+    %% **************************************************
+    % PRE CALCULATIONS FOR HH
+    %**************************************************
+    Tn_layers = length(sizes);     %Number of layers
+    Tn_wfs = length(WFS_angles);   %Number of WFS
+    n_subaps = sizes(1)^2;     %Number of subapertures
+    for j = 1:Tn_wfs
+        for i = 2:Tn_layers  %skip ground layer
+            x_shift(j,i-1) = tan(WFS_angles(j,1)*pi/648000)*alt_p(i);    %Shifts in higher layers
+            y_shift(j,i-1) = tan(WFS_angles(j,2)*pi/648000)*alt_p(i);
+        end
+    end
+
+    %% **************************************************
+    % PROCESS COVARIANCE MATRICES
+    %**************************************************
+
+    for i = 1:length(sizes)
+        Cov_M_norm{i} = Cov_M{i}/Cov_M{i}(1,1)*frac(i); %###Verificar se essa e a posicao certa para layer fraction
+        L{i} = chol(Cov_M_norm{i});    % Cholesky decomposition
+    end
+
+
+    % Full system matrix
+    Hmtx = zeros(WFS_size^2*Tn_wfs,sum(sizes.^2));
+    fprintf('Creating full system Matrix\nIteration:            ');
+    for i=1:sum(sizes.^2)
+        fprintf('\b\b\b\b\b\b\b\b\b\b\b%05d/%05d',i,sum(sizes.^2));
+        layers = zeros(sum(sizes.^2));
+        layers(i) = 1;
+        Hmtx(:,i) = HHmex3(layers,x_shift,y_shift,sizes);
+    end
+    fprintf('\n\n');
+
+    %Invert Covariances for use in reconstruction
+
+    for i = 1:length(sizes)
+        Cov_M_norm_{i} = Cov_M_norm{i}^-1;
+    end
+
+    Cmtx_ = blkdiag(Cov_M_norm_{:});
+    Cmtx = blkdiag(Cov_M_norm{:});
+
+    %Create Kernel for Non Inverse Covariance
+    temp = zeros(layer_size^2,1);
+    temp((layer_size^2+1)/2) = 1;
+
+    [X, Y] = meshgrid(-sizes(end):sizes(end),-sizes(end):sizes(end));
+    Z = sqrt(X.^2 + Y.^2);
+    Z = Z.*tel_diam./layer_size + mindist;
+    for i = 1:sizes(end)*2
+        for j = 1:sizes(end)*2
+            CovKernel(j,i) = c*(2*pi*Z(j,i)*f0)^(5/6)*besselk(5/6,2*pi*f0*Z(j,i));
+        end
+    end
+    CovKernel = CovKernel./(max(max(CovKernel)));
+
+    file_name = sprintf("paperInit_%.2d",size_i);
+    save(file_name);
+end
---
+++ paper.m
@@ -0,0 +1,203 @@
+clear; close all;
+
+size_i = 3;
+file_name = sprintf("paperInit_%.2d",size_i);
+load(file_name);
+
+%%
+for i = 1:length(sizes)
+    tempX{i} = randn(1,sizes(i)^2)*L{i};
+    tempY{i} = randn(1,sizes(i)^2)*L{i};
+end
+layersX = [tempX{:}]';     %X layers with random slopes
+layersY = [tempY{:}]';     %Y layers with random slopes
+
+
+
+
+%%**************************************************
+% simulate WFS
+%**************************************************
+[WFSX] = HHmex3(layersX,x_shift,y_shift,sizes);
+[WFSY] = HHmex3(layersY,x_shift,y_shift,sizes);
+
+
+%%**************************************************
+% RECONSTRUCTION OF THE LAYERS
+%****************************************************
+
+% Add some noise
+sigma_2 = var(WFSX)/1;
+%sigma_2 = 0;
+WFSX = WFSX + sqrt(sigma_2)*randn(size(WFSX));
+WFSY = WFSY + sqrt(sigma_2)*randn(size(WFSY));
+
+%%
+
+%Matrix
+% fprintf('\nReconstructing. Matrix\n');
+% HtH = Hmtx'*Hmtx + sigma_2*Cmtx_;
+% tic;
+% bb = Hmtx'*WFSX;
+% layersX_hat = pcg(HtH,bb,1e-6,1000);
+% bb = Hmtx'*WFSY;
+% layersY_hat = pcg(HtH,bb,1e-6,1000);
+% Time_Matrix = toc;
+% fprintf('Elapsed Time = %f\n',Time_Matrix);
+
+%Classic MVM
+fprintf('\nReconstructing. MVM\n');
+HtH = Hmtx'*Hmtx + sigma_2*Cmtx_;
+Rmat = inv(HtH)*Hmtx';
+tic;
+%layersX_hat = pcg(HtH,bb,1e-6,1000);
+layersX_hat = Rmat*WFSX;
+%layersY_hat = pcg(HtH,bb,1e-6,1000);
+layersY_hat = Rmat*WFSY;
+Time_MVM = toc;
+fprintf('Elapsed Time = %f\n',Time_MVM);
+
+%Matrix without inverting Cov
+fprintf('\nReconstructing. Matrix without inverting Cov\n');
+A = Hmtx*Cmtx*Hmtx';
+A = A + sigma_2*eye(size(A));
+tic;
+layersX_hat2 = Cmtx*Hmtx'*pcg(A,WFSX,1e-6,1000);
+layersY_hat2 = Cmtx*Hmtx'*pcg(A,WFSY,1e-6,1000);
+Time_Matrix = toc;
+fprintf('Elapsed Time = %f\n',Time_Matrix);
+
+%Filter without inverting Cov
+fprintf('\nReconstructing. Filter without inverting Cov\n');
+AA = @(x) HHmex3(Cparmex3(HHTmex3(x,x_shift,y_shift,sizes),sizes,frac,CovKernel),x_shift,y_shift,sizes) + sigma_2*x;
+tic;
+temp = pcg(AA,WFSX,1e-6,1000);
+layersX_hat3 = Cparmex3(HHTmex3(temp,x_shift,y_shift,sizes),sizes,frac,CovKernel);
+temp = pcg(AA,WFSY,1e-6,1000);
+layersY_hat3 = Cparmex3(HHTmex3(temp,x_shift,y_shift,sizes),sizes,frac,CovKernel);
+Time_Fil = toc;
+fprintf('Elapsed Time = %f\n',Time_Fil);
+
+
+%% **************************************************
+% SLOPE ERROR RESULTS
+%****************************************************
+
+TargetX = HH_projection3(layersX,x_shift,y_shift,sizes,alt_p,WFS_target);
+TargetY = HH_projection3(layersY,x_shift,y_shift,sizes,alt_p,WFS_target);
+
+%MVM
+TargetX_hat = HH_projection3(layersX_hat,x_shift,y_shift,sizes,alt_p,WFS_target);
+Error_TargetX_MVM = rms(TargetX-TargetX_hat)
+Error_layersX_MVM = rms(layersX-layersX_hat)
+TargetY_hat = HH_projection3(layersY_hat,x_shift,y_shift,sizes,alt_p,WFS_target);
+Error_TargetY_MVM = rms(TargetY-TargetY_hat);
+Error_layersY_MVM = rms(layersY-layersY_hat);
+
+%Matrix
+TargetX_hat2 = HH_projection3(layersX_hat2,x_shift,y_shift,sizes,alt_p,WFS_target);
+Error_TargetX_Mat = rms(TargetX-TargetX_hat2)
+Error_layersX_Mat = rms(layersX-layersX_hat2)
+TargetY_hat2 = HH_projection3(layersY_hat2,x_shift,y_shift,sizes,alt_p,WFS_target);
+Error_TargetY_Mat = rms(TargetY-TargetY_hat2);
+Error_layersY_Mat = rms(layersY-layersY_hat2);
+
+%Filter without inverting C
+TargetX_hat3 = HH_projection3(layersX_hat3,x_shift,y_shift,sizes,alt_p,WFS_target);
+Error_TargetX_FilterNI = rms(TargetX-TargetX_hat3)
+Error_layersX_FilterNI = rms(layersX-layersX_hat3)
+TargetY_hat3 = HH_projection3(layersY_hat3,x_shift,y_shift,sizes,alt_p,WFS_target);
+Error_TargetY_FilterNI = rms(TargetY-TargetY_hat3);
+Error_layersY_FilterNI = rms(layersY-layersY_hat3);
+
+
+%% **************************************************
+% WFE RESULTS
+%****************************************************
+%Based on the expected tilt removed WFE -> Eq. 9.11, pag 316 Hardy
+expected_WFE = 0.141*(tel_diam/r0)^(5/3)*500/(2*pi);    %In Nanometers
+d = tel_diam/WFS_size;
+rec_target = WFreconWFS(TargetX,TargetY,sizes,d,0.005);
+scaling_value = expected_WFE/rms(rec_target(:));
+rec_target = rec_target*scaling_value;
+
+[temp1, temp2, temp3] = WFrecon(layersX,layersY,sizes,d,0.005);
+rec_layers = [temp1; temp2; temp3]*scaling_value;
+
+%MVM
+[temp1, temp2, temp3] = WFrecon(layersX_hat,layersY_hat,sizes,d,0.005);
+rec_layers_hat = [temp1; temp2; temp3]*scaling_value;
+WFE_layers_MVM = rms(rec_layers(:) - rec_layers_hat(:))
+rec_target_hat = WFreconWFS(TargetX_hat,TargetY_hat,sizes,d,0.005)*scaling_value;
+WFE_Target_MVM = rms(rec_target(:) - rec_target_hat(:))
+
+%Matrix without inverting Cov
+[temp1, temp2, temp3] = WFrecon(layersX_hat2,layersY_hat2,sizes,d,0.005);
+rec_layers_hat2 = [temp1; temp2; temp3]*scaling_value;
+WFE_layers_Mat = rms(rec_layers(:) - rec_layers_hat2(:))
+rec_target_hat2 = WFreconWFS(TargetX_hat2,TargetY_hat2,sizes,d,0.005)*scaling_value;
+WFE_Target_Mat = rms(rec_target(:) - rec_target_hat2(:))
+
+%Filter without inverting C
+[temp1, temp2, temp3] = WFrecon(layersX_hat3,layersY_hat3,sizes,d,0.005);
+rec_layers_hat3 = [temp1; temp2; temp3]*scaling_value;
+WFE_layers_FilterNI = rms(rec_layers(:) - rec_layers_hat3(:))
+rec_target_hat3 = WFreconWFS(TargetX_hat3,TargetY_hat3,sizes,d,0.005)*scaling_value;
+WFE_Target_FilterNI = rms(rec_target(:) - rec_target_hat3(:))
+
+
+
+%%
+%
+%**************************************************
+% VISUAL COMPARISON
+%**************************************************
+Target_vis = zeros(sizes(1));
+Target_vis(subap_index) = TargetX;
+figure;
+imagesc(real(Target_vis));
+title('Target original');
+Target_hat_vis = zeros(sizes(1));
+Target_hat_vis(subap_index) = TargetX_hat3;
+figure;
+imagesc(real(Target_hat_vis));
+title('Target reconstructed');
+
+
+%layer 1
+figure;
+imagesc(real(reshape(layersX(1:sizes(1)^2),sizes(1),[])))
+title('layer 1 original');
+figure;
+imagesc(real(reshape(layersX_hat3(1:sizes(1)^2),sizes(1),[])))
+title('layer 1 rec');
+
+
+%layer 2
+figure;
+imagesc(real(reshape(layersX(sizes(1)^2+1:sizes(1)^2+sizes(2)^2),sizes(2),[])))
+title('layer 2 original');
+figure;
+imagesc(real(reshape(layersX_hat3(sizes(1)^2+1:sizes(1)^2+sizes(2)^2),sizes(2),[])))
+title('layer 2 rec');
+
+%Target Wavefront
+figure;
+imagesc(reshape(rec_target(1:(sizes(1)+1)^2),sizes(1)+1,[]))
+title('Target WF original');
+figure;
+imagesc(reshape(rec_target_hat3(1:(sizes(1)+1)^2),sizes(1)+1,[]))
+title('Target WF rec');
+
+%layer 1 Wavefront
+figure;
+imagesc(reshape(rec_layers(1:(sizes(1)+1)^2),sizes(1)+1,[]))
+title('layer 1 WF original');
+figure;
+imagesc(reshape(rec_layers_hat3(1:(sizes(1)+1)^2),sizes(1)+1,[]))
+title('layer 1 WF rec');
+
+%%
+
+file_name = sprintf("paper_%.2d",size_i);
+save(file_name);
